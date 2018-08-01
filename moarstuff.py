9bc27e062fa859011dbdfaac8c24047bae05830f
Anthony Mellinger <jahagwa@gmail.com>
2018-08-01 14:04:20 -0700

    more practice

---
+++ moarstuff.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+#This progam asks for my name
+print('Yo, sup?')
+print ('What\'s your name?')
+myName = input()
+print('cool, nice to meet you, ' + myName)
+print('How old are you?')
+myAge = input()
+print ('You will be ' + str(int(myAge) + 5) + ' in 5 years')
--- stuff.py
+++ stuff.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3.6
 crap = 'crapofdoom'
 print(crap + 'stuff' + str(42))
+print(str(-3.14))
+print(int(-3.14))
+print(float(-3.14))
